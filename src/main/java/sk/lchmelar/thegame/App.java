56014af072e6c13d255d451269a51cb68f83e796
LukasCh <lukchmelar@gmail.com>
2013-11-30 22:06:21 +0100

    Initial import

--- src/main/java/sk/lchmelar/thegame/App.java
+++ src/main/java/sk/lchmelar/thegame/App.java
@@ -1,13 +1,9 @@
 package sk.lchmelar.thegame;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	new TheGame().run();
     }
 }
---
+++ src/main/java/sk/lchmelar/thegame/entities/GameWorld.java
@@ -0,0 +1,68 @@
+package sk.lchmelar.thegame.entities;
+
+import java.awt.Graphics2D;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import sk.lchmelar.thegame.graphics.Viewport;
+import sk.lchmelar.thegame.interfaces.GameEntity;
+
+public class GameWorld extends GameEntity {
+	private BigDecimal x;
+	private BigDecimal y;
+	private BigDecimal width;
+	private BigDecimal height;
+	
+	private ArrayList<GameEntity> entities = new ArrayList<GameEntity>();
+	
+	public GameWorld () {
+		super();
+		entities.add(new Terrain());
+	}
+	
+	public void render(Graphics2D g, Viewport v) {
+		synchronized(entities){
+			for(GameEntity entity: entities) {
+				entity.render(g, v);
+			}		
+		}
+	}
+
+	public void update(int delta) {
+		synchronized(entities){
+			for(GameEntity entity: entities) {
+				entity.update(delta);
+			}
+		}
+	}
+
+	public BigDecimal getX() {
+		return x;	
+	}	
+	
+	public BigDecimal getY() {
+		return y;	
+	}
+
+	public BigDecimal getWidth() {
+		return width;
+	}
+
+	public BigDecimal getHeight() {
+		return height;
+	}
+
+	public ArrayList<GameEntity> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(ArrayList<GameEntity> entities) {
+		this.entities = entities;
+	}	
+	
+	public void addEntity(GameEntity entity) {
+		synchronized(entities){
+			entities.add(entity);
+		}
+	}	
+}
