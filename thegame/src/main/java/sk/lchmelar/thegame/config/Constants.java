afcf798515a54c0c89538061d7a08a29493bb39c
LukasCh <lukchmelar@gmail.com>
2013-11-30 21:54:02 +0100

    DELETE

--- thegame/src/main/java/sk/lchmelar/thegame/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package sk.lchmelar.thegame.config;
-
-public class Constants {
-	public static final Integer centerX = 10000/2;
-	public static final Integer centerY = 10000/2;
-	public static final Integer cameraSpeed = 100;
-	public static final Integer gravAcc = -10;
-}
