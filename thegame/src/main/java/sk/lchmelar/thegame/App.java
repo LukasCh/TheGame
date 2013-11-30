36fd399df632a8fad9bb4e1f1327da2cb310f999
LukasCh <lukchmelar@gmail.com>
2013-11-30 22:01:35 +0100

    Initial import

---
+++ thegame/src/main/java/sk/lchmelar/thegame/App.java
@@ -0,0 +1,13 @@
+package sk.lchmelar.thegame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
